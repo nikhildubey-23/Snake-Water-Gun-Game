4685830799a6d17ee60b1df9d9bb585b01eac5ee
nikhildubey-23 <ravannick0089@gmail.com>
2024-10-16 18:06:34 +0530

    declaring the variable

---
+++ main.py
@@ -0,0 +1,15 @@
+#we use random module
+import random
+#printing the game name
+print("Snake Water Gun!")
+#number of rounds
+n=int(input("Enter Number of Rounds\n"))
+#setting the options
+options = ['s','g','w']
+#computer marks
+computer_wins = 0
+#user marks
+user_wins = 0
+#number of rounds
+rounds = 1
+
