46c44a150daa728bc5e76ec970173c30ba09285b
nikhildubey-23 <ravannick0089@gmail.com>
2024-10-16 18:25:50 +0530

    completing the game

--- main.py
+++ main.py
@@ -12,4 +12,56 @@ computer_wins = 0
 user_wins = 0
 #number of rounds
 rounds = 1
+#setting the loop till user_range
+while rounds <= n:
+    #user message
+    print(f"Round:{rounds}\nSnake-'s'\nWater-'w'\nGun-'g'")
+    #user input
+    try:
+        player = input("Enter your option:-")
+    except:
+        print("Invalid input!!!!!!!!!!")
 
+    #checking the user input
+    if player != 's' and player != 'g' and player != 'w':
+        print("Invalid input , Enter the right value!")
+        continue
+    #computer will choice its value
+    computer = random.choice(options)
+    # game logic
+    if computer == 's':
+        if player == 'w':
+            computer_wins +=1
+        elif player == 'g':
+            user_wins += 1
+    elif computer == 'w':
+        if player == 's':
+            user_wins += 1
+        elif player == 'g':
+            computer_wins += 1
+    elif computer == 'g':
+        if player == 's':
+            computer_wins +=1
+        elif player == 'w':
+            user_wins += 1
+            
+    # winner Announcement
+    if user_wins > computer_wins:
+        print("User is a winner!")
+    elif computer_wins > user_wins:
+        print("Computer is a winner!")
+    else:
+        print("Match is Draw!!")
+    #breaking the loop
+    rounds += 1   
+#final Winner
+if user_wins > computer_wins:
+    print("User is a winner!")
+elif computer_wins > user_wins:
+    print("Computer is a winner!")
+else:
+    print("Match is Draw!!")
+       
+        
+    
+    
